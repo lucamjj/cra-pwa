f043306aa2e99a217a14d794f704d2b7f5d24e6c
Luca Carangella <luca.carangella@gmail.com>
2018-10-11 16:43:49 +0100

    Updates

---
+++ precache-manifest.6dbad29a6767f576baa336aae7173f36.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "67c1cd3a9debd6d5bf4e",
+    "url": "/cra-pwa/static/js/runtime~main.67c1cd3a.js"
+  },
+  {
+    "revision": "1e33eadba5e04d290d94",
+    "url": "/cra-pwa/static/js/main.1e33eadb.chunk.js"
+  },
+  {
+    "revision": "7eae6140ad0adf38fbdc",
+    "url": "/cra-pwa/static/js/1.7eae6140.chunk.js"
+  },
+  {
+    "revision": "1e33eadba5e04d290d94",
+    "url": "/cra-pwa/static/css/main.03dd5399.chunk.css"
+  },
+  {
+    "revision": "2772857053ea51b0cee0642137ad6ec8",
+    "url": "/cra-pwa/index.html"
+  }
+];
